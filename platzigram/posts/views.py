3863d2c1b66beb0ef4f30a44425a17dace16b67b
José Ricardo Pérez <ararage@Joses-MacBook-Pro.local>
2019-05-10 00:00:04 -0500

    Added Class Views URLS

--- platzigram/posts/views.py
+++ platzigram/posts/views.py
@@ -1,6 +1,9 @@
 # Django
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
 # Forms
 from posts.forms import PostForm
@@ -12,13 +15,12 @@ from posts.models import Post
 from datetime import datetime
 
 
-@login_required
-def list_posts(request):
-    """
-        List existing posts
-    """
-    posts = Post.objects.all().order_by('-created')
-    return render(request, 'posts/feed.html', {'posts': posts})
+class PostsFeedView(LoginRequiredMixin, ListView):
+    template_name = 'posts/feed.html'
+    model = Post
+    ordering = ('-created')
+    paginate_by = 2
+    context_object_name = 'posts'
 
 
 @login_required
@@ -28,7 +30,7 @@ def create_post(request):
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('feed')
+            return redirect('posts:feed')
     else:
         form = PostForm()
     return render(
--- platzigram/users/urls.py
+++ platzigram/users/urls.py
@@ -1,28 +1,31 @@
 from django.urls import path
-from django.views.generic import TemplateView
-
 from users import views
 
 
 urlpatterns = [
     path(
+        route='login/',
+        view=views.LoginView.as_view(),
+        name='login'
+    ),
+    path(
+        route='logout/',
+        view=views.LogoutView.as_view(),
+        name='logout'
+    ),
+    path(
+        route='signup/',
+        view=views.SignupView.as_view(),
+        name='signup'
+    ),
+    path(
+        route='me/profile/',
+        view=views.UpdateProfileView.as_view(),
+        name='update'
+    ),
+    path(
         route='<str:username>/',
-        view=TemplateView.as_view(template_name='users/detail.html'),
+        # TemplateView.as_view(template_name='users/detail.html'),
+        view=views.UserDetailView.as_view(),
         name='detail'),
-    path(
-        route='users/login/',
-        view=views.login_view,
-        name='login'),
-    path(
-        route='users/logout/',
-        view=views.logout_view,
-        name='logout'),
-    path(
-        route='users/signup/',
-        view=views.signup,
-        name='signup'),
-    path(
-        route='users/me/profile/',
-        view=views.update_profile,
-        name='update_profile'),
 ]
